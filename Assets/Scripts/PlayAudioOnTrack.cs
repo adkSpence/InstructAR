d8d76bb99e07d74c699a7c5f8ac92ac10c98bd36
Spencer Apeadjei-Duodu <spencerapeadjei@gmail.com>
2025-07-23 17:47:52 +0200

    Audios added to feature-assembly: WIP 17:47 Wed 23

---
+++ Assets/Scripts/PlayAudioOnTrack.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using Vuforia;
+
+[RequireComponent(typeof(ObserverBehaviour), typeof(AudioSource))]
+public class PlayAudioOnTrack : MonoBehaviour
+{
+    AudioSource _audio;
+    ObserverBehaviour _observer;
+
+    void Awake()
+    {
+        // Grab references
+        _audio = GetComponent<AudioSource>();
+        _observer = GetComponent<ObserverBehaviour>();
+    }
+
+    void OnEnable()
+    {
+        // Register to Vuforia’s status‐changed event
+        _observer.OnTargetStatusChanged += OnStatusChanged;
+    }
+
+    void OnDisable()
+    {
+        _observer.OnTargetStatusChanged -= OnStatusChanged;
+    }
+
+    void OnStatusChanged(ObserverBehaviour behaviour, TargetStatus status)
+    {
+        if ((status.Status == Status.TRACKED || status.Status == Status.EXTENDED_TRACKED) && !_audio.isPlaying)
+        {
+            _audio.Play();
+        }
+        else if (status.Status == Status.NO_POSE && _audio.isPlaying)
+        {
+            _audio.Stop();
+        }
+    }
+}
